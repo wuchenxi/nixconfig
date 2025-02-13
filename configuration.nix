193434f7d98f240d9f8429595508d3af4984243c
Chenxi Wu <cw538@cornell.edu>
2025-02-13 15:27:40 -0600

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -17,8 +17,13 @@
   services.fstrim.enable = true;
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
